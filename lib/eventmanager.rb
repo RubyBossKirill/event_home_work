6ce539cd5565200bddf8c12e81e930e7bba13cdd
LonelyWell <kirill_richardson@mail.ru>
2024-02-22 13:05:31 +0500

    Добавление процесса Аунтентификации пользователя.

--- lib/eventmanager.rb
+++ lib/eventmanager.rb
@@ -4,11 +4,14 @@ require_relative 'event'
 
 class EventManager
     attr_reader :username, :name, :surname, :email, :to_h
-    def initialize
+    def initialize(username)
         @events = {}
         @participants = {}
         @json_hash = {}
         load_participant
+        if participant_exist?(username)
+            authentication_participant(username)
+        end
     end
 
     def add_participant(username, name, surname, email)
@@ -25,7 +28,18 @@ class EventManager
 
         p "#{username} удален из списка"
     end
+
+
     private
+    
+    def participant_exist?(username)
+        @json_hash.key?(username)
+    end
+
+    def authentication_participant(username)
+        user_participant = Participant.new(username, @json_hash[username]['name'], @json_hash[username]['surname'], @json_hash[username]['email'])
+        @participants[username] = user_participant
+    end
 
     def load_participant 
         if File.exist?('../data/data_participant.json')
@@ -54,11 +68,4 @@ class EventManager
     end
 end
 
-user = EventManager.new
-
-user.add_participant("DianaCher", "Diana", "Test", "test4@gmail.com")
-user.add_participant("LonelyWell", "Kirilll", "Elin", "test@gmail.com")
-user.add_participant("Dimitro", "Dima", "Test", "test2@gmail.com")
-
-
-user.remove_participant("LonelyWell")
+user = EventManager.new("Dimitro")
