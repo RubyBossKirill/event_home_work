1ebe994a7d0485fb1bf0386400bee0ff885cfbdc
LonelyWell <kirill_richardson@mail.ru>
2024-02-22 12:29:53 +0500

    Добавление remove participant и save json hash.

--- lib/eventmanager.rb
+++ lib/eventmanager.rb
@@ -19,6 +19,12 @@ class EventManager
         puts "Участник #{name} добавлен"
     end
 
+    def remove_participant(username)
+        @json_hash.delete(username)
+        save_json_hash
+
+        p "#{username} удален из списка"
+    end
     private
 
     def load_participant 
@@ -31,9 +37,17 @@ class EventManager
             end
         end
     end
+
     def save_participant(username)
         File.open('../data/data_participant.json', 'w') do |file|
-            @json_hash[username] = @json_hash[username].to_h
+            @json_hash[username] = @participants[username].to_h
+            json_data = @json_hash.to_json
+            file.write(json_data)
+        end
+    end
+
+    def save_json_hash
+        File.open('../data/data_participant.json', 'w') do |file|
             json_data = @json_hash.to_json
             file.write(json_data)
         end
@@ -42,4 +56,9 @@ end
 
 user = EventManager.new
 
+user.add_participant("DianaCher", "Diana", "Test", "test4@gmail.com")
 user.add_participant("LonelyWell", "Kirilll", "Elin", "test@gmail.com")
+user.add_participant("Dimitro", "Dima", "Test", "test2@gmail.com")
+
+
+user.remove_participant("LonelyWell")
